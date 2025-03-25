ed8996bdb5417370f857672e0614eba8ca88438b
Lea Michel <lm.michel63@gmail.com>
2025-03-25 17:01:57 +0100

    feat: implement client

--- src/Client.java
+++ src/Client.java
@@ -2,5 +2,30 @@
 import java.net.*;
 
 public class Client {
+    public Client() {
+    }
 
+    public void run(){
+        try {
+            // Création du canal
+            DatagramSocket socketClient = new DatagramSocket();
+            InetAddress adresseClient = InetAddress.getByName("localhost");
+            byte[] envoyees;
+            byte[] recues = new byte[1024];
+            // Émettre
+            String message = "Bonjour !";
+            envoyees = message.getBytes();
+            DatagramPacket messageEnvoye = new DatagramPacket(envoyees, envoyees.length, adresseClient, 6666);
+            socketClient.send(messageEnvoye);
+            // Recevoir
+            DatagramPacket paquetRecu = new DatagramPacket(recues, recues.length);
+            socketClient.receive(paquetRecu);
+            String reponse = new String(paquetRecu.getData(), 0, paquetRecu.getLength());
+            System.out.println("Depuis le serveur: " + reponse);
+            // Libérer le canal
+            socketClient.close();
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }
 }
