92b6a1c54ac4f80d6a08356c9f11ccaf4668b764
Louka <louka.pesic@gmail.com>
2025-03-25 16:59:39 +0100

    Ajout serv.run() dans le main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Serveur serv = new Serveur();
+        serv.run();
+
 
     }
 }
