7debfc5f2c3f7d76d5fc02e73cb4214019cfe84b
Louka <louka.pesic@gmail.com>
2025-03-31 15:30:58 +0200

    feat : migration de la gestion des ports et adresses vers ClientHandler

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,10 +1,19 @@
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientHandler implements Runnable {
     private DatagramSocket socket;
+    public static Map<String, ClientInfo> clients = new HashMap<>();  // Mapping des clients par identifiant
+    
 
     public ClientHandler(DatagramSocket socket) {
+        
         this.socket = socket;
+    }
+
+    public void addClient(String clientId, DatagramPacket packetRecu) {
+        clients.put(clientId, new ClientInfo(packetRecu.getAddress(), packetRecu.getPort()));
     }
 
     @Override
@@ -19,6 +28,24 @@ public class ClientHandler implements Runnable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public static class ClientInfo {
+        private InetAddress address;
+        private int port;
+
+        public ClientInfo(InetAddress address, int port) {
+            this.address = address;
+            this.port = port;
+        }
+
+        public InetAddress getAddress() {
+            return address;
+        }
+
+        public int getPort() {
+            return port;
         }
     }
 }
